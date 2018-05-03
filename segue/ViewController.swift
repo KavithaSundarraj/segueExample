2a94b338c8cd95c07a4b9c893c2f256615626b51
DSV <dsv@eduroam-10-200-43-179.local.su.se>
2018-05-03 13:15:09 +0200

    commit 1 - Deleted un necessary comments

--- segue/ViewController.swift
+++ segue/ViewController.swift
@@ -13,12 +13,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var dataTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+     
+    
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "dataSegue" {
